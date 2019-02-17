99a73cd311f14dbce3ebd92b08da4fe8c58d4374
harsh chaurasiya <harshchaurasiya6768@gmail.com>
2019-02-17 17:57:39 +0530

    collapsing toolbar added

---
+++ collapsingtoolbar/src/main/java/com/technohack/collapsingtoolbar/CollapsingToolbar.java
@@ -0,0 +1,21 @@
+package com.technohack.collapsingtoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class CollapsingToolbar extends AppCompatActivity {
+
+    Toolbar  toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_toolbar);
+
+        toolbar=findViewById(R.id.toolbarId);
+        setSupportActionBar(toolbar);
+
+       if(getSupportActionBar()!=null)
+           getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
